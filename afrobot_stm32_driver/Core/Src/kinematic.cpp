6fa0463a4f903df9461f9bc7dcb90cb61a8a5165
krjar <kr.jaro77@gmail.com>
2023-03-15 13:53:06 +0100

    Forward kinematics calculation

--- afrobot_stm32_driver/Core/Src/kinematic.cpp
+++ afrobot_stm32_driver/Core/Src/kinematic.cpp
@@ -12,14 +12,24 @@
 #include <kinematic.hpp>
 
 
-#define WHEEL_DIAMETER						97		// mm
-#define WHEEL_SEPARATION_HORIZONTAL			360		// mm   ---
-#define WHEEL_SEPARATION_VERTICAL			300		// mm   |
+#define WHEEL_DIAMETER						97		// in mm
+#define WHEEL_SEPARATION_HORIZONTAL			180		// robot center to wheel axis in mm   ---
+#define WHEEL_SEPARATION_VERTICAL			150		// robot center to wheel axis in mm   |
 
 
 double * getForwardKinematics(geometry_msgs::TwistStamped *cmd)
 {
-	double w[4];
+	static double w[4]; // w[0] = FR, w[1] = FL, w[2] = BR, w[3] = BL
+	double vx, vy, wz;
+
+	vx = cmd->twist.linear.x ;
+	vy = cmd->twist.linear.y ;
+	wz = cmd->twist.angular.z ;
+
+	w[0] = (1/WHEEL_DIAMETER) * (vx + vy + (WHEEL_SEPARATION_HORIZONTAL + WHEEL_SEPARATION_VERTICAL) * wz);
+	w[1] = (1/WHEEL_DIAMETER) * (vx - vy - (WHEEL_SEPARATION_HORIZONTAL + WHEEL_SEPARATION_VERTICAL) * wz);
+	w[2] = (1/WHEEL_DIAMETER) * (vx - vy + (WHEEL_SEPARATION_HORIZONTAL + WHEEL_SEPARATION_VERTICAL) * wz);
+	w[3] = (1/WHEEL_DIAMETER) * (vx + vy - (WHEEL_SEPARATION_HORIZONTAL + WHEEL_SEPARATION_VERTICAL) * wz);
 
 	return w ;
 }
