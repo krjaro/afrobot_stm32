d955683522e012bd73eb108b5a7c267fdab8a718
krjar <kr.jaro77@gmail.com>
2023-03-27 12:38:16 +0200

    Communication functions definitions

--- afrobot_stm32_driver/Core/Inc/comms.hpp
+++ afrobot_stm32_driver/Core/Inc/comms.hpp
@@ -8,8 +8,21 @@
 #ifndef INC_COMMS_HPP_
 #define INC_COMMS_HPP_
 
+#include <main.hpp>
+#include <ros.h>
+#include <FreeRTOS.h>
+#include <queue.h>
 
 
+typedef struct
+{
+	QueueHandle_t *cmdQHandle ;
+}comms;
+
+
+void commsInit();
+void commsSetup(void);
+void commsLoop(void);
 
 
 #endif /* INC_COMMS_HPP_ */
