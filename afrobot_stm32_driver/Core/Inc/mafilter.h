897d6fea1bbdfa52fffc7b1416cb991ebe3bb39f
krjar <kr.jaro77@gmail.com>
2023-05-03 12:10:12 +0200

    Files for moving average filter implementation

---
+++ afrobot_stm32_driver/Core/Inc/mafilter.h
@@ -0,0 +1,13 @@
+/*
+ * mafilter.h
+ *
+ *  Created on: May 3, 2023
+ *      Author: krjar
+ */
+
+#ifndef INC_MAFILTER_H_
+#define INC_MAFILTER_H_
+
+
+
+#endif /* INC_MAFILTER_H_ */
---
+++ afrobot_stm32_driver/Core/Src/mafilter.c
@@ -0,0 +1,8 @@
+/*
+ * mafilter.c
+ *
+ *  Created on: May 3, 2023
+ *      Author: krjar
+ */
+
+#include <mafilter.h>
